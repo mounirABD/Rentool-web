e7f833837b51a840185203ba69a42a745909cfc4
Mounir_ABD <aabada.mounir@gmail.com>
2018-11-22 20:22:35 +0100

    First commit

---
+++ Rentool-Server-Spring/src/main/java/com/mql/RentoolBootstraper/RentoolSpringAngularApplication.java
@@ -0,0 +1,12 @@
+package com.mql.RentoolBootstraper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentoolSpringAngularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentoolSpringAngularApplication.class, args);
+	}
+}
