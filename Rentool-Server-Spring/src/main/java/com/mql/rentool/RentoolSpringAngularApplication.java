3cf509a95fe1719c8e9eacdd0e0cb16898aa81de
ibra15 <iabdeneby@gmail.com>
2018-11-25 16:38:28 +0100

    + Add SpringBoot Tool + Edit Angular build distination + Edit project structure

--- Rentool-Server-Spring/src/main/java/com/mql/rentool/RentoolSpringAngularApplication.java
+++ Rentool-Server-Spring/src/main/java/com/mql/rentool/RentoolSpringAngularApplication.java
@@ -1,4 +1,4 @@
-package com.mql.RentoolBootstraper;
+package com.mql.rentool;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
